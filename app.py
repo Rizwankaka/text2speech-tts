d9169c216568759711fa57c5bbb741ab29c18139
Rizwan Rizwan <122643396+Rizwankaka@users.noreply.github.com>
2024-03-01 15:57:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,36 +24,6 @@ def text_to_speech(api_key, text: str):
         
         # Return the path to the audio file
         return speech_file_path
-# Set a solid background color
-def set_background_color():
-    st.markdown(
-        """
-        <style>
-        .stApp {
-            background-color: #f0f2f6;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-set_background_color()
-
-# Set a background image
-# def set_background_image():
-#     st.markdown(
-#         """
-#         <style>
-#         .stApp {
-#             background-image: url("https://yourimageurl.com/yourimage.jpg");
-#             background-size: cover;
-#         }
-#         </style>
-#         """,
-#         unsafe_allow_html=True
-#     )
-
-# set_background_image()
 
 # Streamlit UI setup
 st.title("🔊 Text to Speech Converter 📝")
@@ -83,11 +53,12 @@ if st.button("Convert"):
             os.remove(speech_path)
         except Exception as e:
             st.error(f"An error occurred: {e}")
-# Adding the HTML footer
 
-footer_html = """
+# Adding the HTML footer
+# Profile footer HTML for sidebar
+sidebar_footer_html = """
 <div style="text-align: left;">
-    <p style="font-size: 25px;"><b>Author: 🌟 Rizwan Rizwan 🌟</b></p>
+    <p style="font-size: 16px;"><b>Author: 🌟 Rizwan Rizwan 🌟</b></p>
     <a href="https://github.com/Rizwankaka"><img src="https://img.shields.io/badge/GitHub-Profile-blue?style=for-the-badge&logo=github" alt="GitHub"/></a><br>
     <a href="https://www.linkedin.com/in/rizwan-rizwan-1351a650/"><img src="https://img.shields.io/badge/LinkedIn-Profile-blue?style=for-the-badge&logo=linkedin" alt="LinkedIn"/></a><br>
     <a href="https://twitter.com/RizwanRizwan_"><img src="https://img.shields.io/badge/Twitter-Profile-blue?style=for-the-badge&logo=twitter" alt="Twitter"/></a><br>
@@ -95,4 +66,68 @@ footer_html = """
     <a href="mailto:riwan.rewala@gmail.com"><img src="https://img.shields.io/badge/Gmail-Contact%20Me-red?style=for-the-badge&logo=gmail" alt="Gmail"/></a>
 </div>
 """
+
+# Render profile footer in sidebar at the "bottom"
+st.sidebar.markdown(sidebar_footer_html, unsafe_allow_html=True)
+# Set a background image
+def set_background_image():
+    st.markdown(
+        """
+        <style>
+        .stApp {
+            background-image: url("https://images.pexels.com/photos/6847584/pexels-photo-6847584.jpeg");
+            background-size: cover;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+set_background_image()
+
+# Set a background image for the sidebar
+sidebar_background_image = '''
+<style>
+[data-testid="stSidebar"] {
+    background-image: url("https://images.pexels.com/photos/6101958/pexels-photo-6101958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+    background-size: cover;
+}
+</style>
+'''
+
+st.sidebar.markdown(sidebar_background_image, unsafe_allow_html=True)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# Custom CSS to inject into the Streamlit app
+footer_css = """
+<style>
+.footer {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    width: auto;
+    background-color: transparent;
+    color: black;
+    text-align: right;
+    padding-right: 10px;
+}
+</style>
+"""
+
+# HTML for the footer - replace your credit information here
+footer_html = f"""
+<div class="footer">
+    <p>Credit: Dr. Aammar Tufail | Phd | Data Scientist | Bioinformatician (<a href="https://www.youtube.com/@Codanics" target="_blank">CODANICS</a>)</p>
+</div>
+"""
+
+# Combine CSS and HTML for the footer
+st.markdown(footer_css, unsafe_allow_html=True)
 st.markdown(footer_html, unsafe_allow_html=True)
